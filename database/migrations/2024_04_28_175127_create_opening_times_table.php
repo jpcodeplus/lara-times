88d269dc004dc147ad40f467f850eb208511e87d
jpcodeplus <behrens.codeplus@gmail.com>
2024-04-28 19:54:19 +0200

    create tabel

---
+++ database/migrations/2024_04_28_175127_create_opening_times_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('opening_times', function (Blueprint $table) {
+            $table->id();
+            $table->time('open_time');
+            $table->time('close_time');
+            $table->string('day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('opening_times');
+    }
+};
